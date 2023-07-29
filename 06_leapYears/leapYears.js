13959b5d963bfbed22b72989c7dfcf2ae446d992
Omoregha Michael <Regsybaba@Reghas-MacBook-Air.local>
2023-07-29 11:40:13 +0100

    Finsihed EXERCISE-06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(val) {
+//    if (val % 4 === 0){
+//      return true
+//     } else if (val % 4 !== 0){
+//         return false
+//     } else if (val % 100 === 0 && val % 400 === 0){
+//         return true
+//     }else if (90 % 100 === 0 || 90 % 400 !== 0){
+//         return false
+//     }else {return true}
+       
+    return val % 4 === 0 && (val % 100 !== 0) || (val % 400 === 0)
 };
 
 // Do not edit below this line
