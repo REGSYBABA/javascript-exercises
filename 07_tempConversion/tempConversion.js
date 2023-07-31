77a124ebfd49925ad7c193cebccd6351c5a83b48
Omoregha Michael <Regsybaba@Reghas-MacBook-Air.local>
2023-07-31 17:54:41 +0100

    Finshed EXERCISE-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(X) {
+  let choke = (X - 32) * 5 / 9 
+  return Math.round(choke * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Y) { 
+ let val = (Y * 9 / 5 + 32)
+  return Math.round(val * 10) / 10
 };
 
 // Do not edit below this line
