a6c5c31b3b48ad7425d8327640064182e019d56d
REGSYBABA <sumimichael77@gmail.com>
2023-08-10 12:36:28 +0100

    finsished exercise 8

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+   return array.reduce((a, b) => {
+    let lastguy = getAge(a.yearOfDeath, a.yearOfBirth)
+    let newGuy = getAge(b.yearOfDeath, b.yearOfBirth)
+    return lastguy > newGuy ? a : b
+   })
 
-};
+}
+
+const getAge = function(deathDate, birthDate){
+    if (!deathDate){
+        deathDate = new Date().getFullYear()
+    }
+    return deathDate - birthDate
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
